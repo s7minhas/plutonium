ee6f7d75daa87338ae48d58ec1245c95ab56496f
s7m <s7.minhas@gmail.com>
2021-09-08 09:42:08 -0400

    posterior analysis

--- 03_downstreamAnalyses/06_postAnalyses.R
+++ 03_downstreamAnalyses/06_postAnalyses.R
@@ -9,6 +9,22 @@ rpth = paste0(pathDrop, 'results/')
 pkgs = c(
   'lme4', 'rstanarm', 'ggplot2' )
 loadPkg(pkgs)
+
+# mod summ fns
+digs=3
+getCoef = function(x){
+  round(cbind(mu=fixef(x), sd=sqrt(diag(vcov(x)))),digs) }
+
+getCoefB = function(x, vars, int = FALSE){
+  out = cbind(
+    mu=apply(x[,vars], 2, mean) ,
+    sd=apply(x[,vars], 2, sd) )
+  if(int){
+    out = cbind(out,
+      qtLo95 = apply(x[,vars], 2, quantile, .025),
+      qtHi95 = apply(x[,vars], 2, quantile, .975) ) }
+  out = round( out, digs )
+  return(out) }
 ####
 
 ####
@@ -18,9 +34,47 @@ load(paste0(rpth, 'dstreamModels.rda'))
 ####
 
 ####
-summary(m1)
-summary(m1b)
-plot(m1b)
-loadPkg('bayesplot')
-plot(m1b, 'trace', pars=c('(Intercept)', 'USf1.l1', 'polity.l1', 'GDP.l1', 'pop.l1', 'beijDist', 'washDist', 'IdealPointDistance'))
+ivs = c(
+  '(Intercept)', 'USf1.l1', 'polity.l1',
+  'GDP.l1', 'pop.l1',
+  'beijDist', 'washDist',
+  'IdealPointDistance' )
+####
+
+####
+# check convergence
+plot(m1b, 'trace', pars=ivs)
+plot(m2b, 'trace', pars=ivs[-2])
+plot(m3b, 'trace', pars=ivs[-2])
+####
+
+####
+# pull out mats
+bMats = lapply(
+  list(m1b, m2b, m3b, m1bZ, m2bZ, m3bZ),
+  function(x){ as.matrix(x) } )
+names(bMats) = c(
+  paste0('m',1:3,'b'),
+  paste0('m',1:3,'bZ') )
+####
+
+####
+#  quick comparison of iv effs
+getCoef(m1)
+getCoefB(bMats$'m1b', ivs)
+
+getCoef(m2)
+getCoefB(bMats$'m2b', ivs[-2])
+
+getCoef(m3)
+getCoefB(bMats$'m3b', ivs[-2])
+####
+
+####
+# ran effects
+effs = setdiff(colnames(bMats$'m2b'), ivs)
+getCoefB(bMats$'m2b', effs, T)
+
+effs = setdiff(colnames(bMats$'m3b'), ivs)
+getCoefB(bMats$'m3b', effs, T)
 ####
